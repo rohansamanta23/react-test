77cbefc27272c8c1941a0bb58db60721a0b19237
rohan samanta <rohansamanta45@gmail.com>
2024-03-06 22:44:36 +0530

    video no-314

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,31 @@ function App() {
     lName: "",
     email: ""
   });
-
+  function eventHandel(event){
+    const val = event.target.value;
+    const name = event.target.name;
+    setContact(preVal=>{
+        if(name === "fName"){
+            return {
+                fName:val,
+                lName:preVal.lName,
+                email:preVal.email
+            };
+        } else if(name === "lName"){
+            return {
+                fName:preVal.fName,
+                lName:val,
+                email:preVal.email
+            };
+        } else if(name === "email"){
+            return {
+                fName:preVal.fName,
+                lName:preVal.lName,
+                email:val
+            };
+        }
+    });
+  }
   return (
     <div className="container">
       <h1>
@@ -14,9 +38,9 @@ function App() {
       </h1>
       <p>{contact.email}</p>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
-        <input name="email" placeholder="Email" />
+        <input onChange={eventHandel} name="fName" placeholder="First Name" value={contact.fName}/>
+        <input onChange={eventHandel} name="lName" placeholder="Last Name" value={contact.lName}/>
+        <input onChange={eventHandel} name="email" placeholder="Email" value={contact.email}/>
         <button>Submit</button>
       </form>
     </div>
